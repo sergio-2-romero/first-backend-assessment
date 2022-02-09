fcd49b0dda16aff25346551571abd97218e08ea3
sergio-2-romero <sergio.2.romero@atos.net>
2022-02-08 18:23:17 -0600

    First commit

---
+++ src/main/java/com/atos/firstbackend/FirstbackendApplication.java
@@ -0,0 +1,13 @@
+package com.atos.firstbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atos/firstbackend/FirstbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atos.firstbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
